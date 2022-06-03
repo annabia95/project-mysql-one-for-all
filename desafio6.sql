7590ee292d47c1a0aa6ceb67a205f97cf1940890
Anna Beatriz Garcia <annagarcia@id.uff.br>
2022-06-03 15:44:08 -0300

    feat: add requirement 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,7 +1,7 @@
 SELECT 
-	MIN(p.preco) AS 'faturamento_minimo', 
-    MAX(p.preco) AS 'faturamento_máximo',
-	ROUND(AVG(p.preco), 2) AS 'faturamento_medio',
+	  MIN(p.preco) AS 'faturamento_minimo', 
+    MAX(p.preco) AS 'faturamento_maximo',
+	  ROUND(AVG(p.preco), 2) AS 'faturamento_medio',
     SUM(p.preco) AS 'faturamento_total'
 FROM SpotifyClone.planos AS p
 INNER JOIN SpotifyClone.usuarios AS u
